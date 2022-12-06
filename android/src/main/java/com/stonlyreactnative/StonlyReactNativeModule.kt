2c55adade5e43261077a41a8c8c834986291dede
Remy Baroukh <contact@remybaroukh.fr>
2022-12-06 16:27:23 +0100

    Base strcture for react-native sdk

--- android/src/main/java/com/stonlyreactnative/StonlyReactNativeModule.kt
+++ android/src/main/java/com/stonlyreactnative/StonlyReactNativeModule.kt
@@ -20,6 +20,76 @@ class StonlyReactNativeModule(reactContext: ReactApplicationContext) :
     Stonly.onScreenChanged(screenName, index)
   }
 
+  @ReactMethod
+  fun setWidgetId(widgetId: String) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun setDebugEnabled(debugEnabled: Boolean) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun setMonitoringEnabled(debugEnabled: Boolean) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun setSegmentAnonymousId(segmentAnonymousId: String) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun sendData(widgetId: Any) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun clearSentData() {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun setWidgetLanguage(languageCode: String) {
+    Stonly.setWidgetLanguage(languageCode)
+  }
+
+  @ReactMethod
+  fun setWindowLevel(windowLever : Int) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun openGuide(guideId: String, stepId: String, widgetOptions: Any) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun openGuidedTour(guideId: String, stepId: String) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun openKnowledgeBase(teamKnowledgeBaseId: String, folderId: String) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun closeWidget(widgetRuleId: String) {
+    Log.d(NAME, "This function doesn't exist on Android")
+  }
+
+  @ReactMethod
+  fun identify(customerId: String, properties: String) {
+    Stonly.tracker?.identify(customerId)
+  }
+
+  @ReactMethod
+  fun track(eventName: String) {
+    Stonly.tracker?.track(eventName)
+  }
+
   companion object {
     const val NAME = "StonlyReactNative"
 
