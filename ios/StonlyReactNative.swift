26e9c560259f0ceb3beccedc4b19d0f169bb76db
Remy Baroukh <contact@remybaroukh.fr>
2022-12-19 14:02:04 +0100

    Link to function

--- ios/StonlyReactNative.swift
+++ ios/StonlyReactNative.swift
@@ -6,5 +6,6 @@ class StonlyReactNative: NSObject {
     @objc(onScreenChanged:tabIndex:)
     func onScreenChanged(screenName: String, tabIndex: Int) -> Void {
         print("onScreenChanged")
+        Stonly.Widget.didChangeScreen(screenName, tabIndex: tabIndex)
     }
 }
