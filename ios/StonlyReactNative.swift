890925dc3fe2fdef4b7e95b1807d2c7c89af88d3
Remy Baroukh <contact@remybaroukh.fr>
2022-12-23 13:17:09 +0100

    Fix Android Crash - v0.1.3

--- ios/StonlyReactNative.swift
+++ ios/StonlyReactNative.swift
@@ -5,7 +5,6 @@ class StonlyReactNative: NSObject {
     
     @objc(onScreenChanged:tabIndex:)
     func onScreenChanged(screenName: String, tabIndex: Int) -> Void {
-        print("onScreenChanged")
         Stonly.Widget.didChangeScreen(screenName, tabIndex: tabIndex)
     }
 }
