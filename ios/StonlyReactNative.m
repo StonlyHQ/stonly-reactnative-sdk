1586598da7ae0808e3b3cbb7021d0065458c86ac
Remy Baroukh <contact@remybaroukh.fr>
2023-01-12 00:34:14 +0100

    Main queue on ios

--- ios/StonlyReactNative.m
+++ ios/StonlyReactNative.m
@@ -2,6 +2,11 @@
 @import Stonly;
 @interface RCT_EXTERN_MODULE(StonlyReactNative, NSObject)
 
+- (dispatch_queue_t)methodQueue
+{
+  return dispatch_get_main_queue();
+}
+
 RCT_EXTERN_METHOD(onScreenChanged:(NSString*)screenName tabIndex:(int)tabIndex)
 
 RCT_EXPORT_METHOD(openGuidedTour:(NSString*)guideId stepId:(NSString*)stepId)
